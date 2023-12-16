d86c69176b962ea303eb6cad846be0ad26ff1d45
Jiwon Rue Lee <subco.lee@gmail.com>
2023-12-16 23:29:35 +0900

    feat: add sum endpoint

--- 04/main.py
+++ 04/main.py
@@ -1,5 +1,5 @@
 from typing import Union
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
@@ -10,3 +10,10 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.post("/sum")
+def sum(a: int, b: int):
+    # if not isinstance(a, int) or not isinstance(b, int):
+    #     raise HTTPException(status_code=422, detail="[ERROR] a and b must be int")
+    return {"sum": a + b}
